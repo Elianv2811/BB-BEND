3c6c1f3da7f94260a263c48005d1da03dae260b6
Elianv2811 <00227021@uca.edu.sv>
2022-11-29 10:27:23 -0600

    profile

--- routes/api/auth.router.js
+++ routes/api/auth.router.js
@@ -1,17 +1,19 @@
-const Express = require('express');
+const Express = require("express");
 const router = Express.Router();
 
-const authController = require('../../controllers/auth.controller');
-const runValidation = require('../../validators/index.middleware');
-const {registerValidator} = require('../../validators/auth.validators');
+const authController = require("../../controllers/auth.controller");
+const runValidation = require("../../validators/index.middleware");
+const { registerValidator } = require("../../validators/auth.validators");
 
-router.post("/signup",
-    registerValidator,
-    runValidation,
-    authController.register
-    );
+router.post(
+  "/signup",
+  registerValidator,
+  runValidation,
+  authController.register
+);
 
-router.post("/signin", 
-authController.login);
+router.post("/signin", authController.login);
+
+router.get("/profile");
 
 module.exports = router;
