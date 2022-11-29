79ac5562a748fef91311873b23b0f697e69dfc33
Elianv2811 <00227021@uca.edu.sv>
2022-11-29 11:31:34 -0600

    user

--- routes/api/user.router.js
+++ routes/api/user.router.js
@@ -3,13 +3,7 @@ const router = Express.Router();
 
 const userController = require("../../controllers/user.controller");
 const runValidation = require("../../validators/index.middleware");
-const userValidator = require("../../validators/user.validators");
 
-router.get(
-  "/profile",
-  userValidator.findUserByIdValidator,
-  runValidation,
-  userController.findOneByID
-);
+router.get("/profile", runValidation, userController.findOneByID);
 
 module.exports = router;
